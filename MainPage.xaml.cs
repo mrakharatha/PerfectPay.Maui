4e2e022a92ce063cf7526c84a32dde02fe5a05aa
seyedmohammadreza <59357443+mrakharatha@users.noreply.github.com>
2023-10-27 17:42:06 +0330

    Making the appearance of the application

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -8,5 +8,20 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
+	private void TxtBill_OnCompleted(object sender, EventArgs e)
+	{
+	}
+
+	private void Slider_OnValueChanged(object sender, ValueChangedEventArgs e)
+	{
+	}
+
+	private void BtnMinus_OnClicked(object sender, EventArgs e)
+	{
+	}
+
+	private void BtnPlus_OnClicked(object sender, EventArgs e)
+	{
+	}
 }
 
